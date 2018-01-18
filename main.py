ba2f6615d94bb9cdfd5a52cfe5dbfd4ff5db702b
garci66 <garci66@gmail.com>
2018-01-18 20:51:33 -0300

    Better handling of server connection errors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dateutil import parser
 from pyzabbix import ZabbixMetric, ZabbixSender
+from socket import error as socket_error
 
 import fastdotcom
 import time
@@ -16,7 +17,7 @@ test_duration = int(os.getenv("TEST_DURATION", 6))
 zabbix_server = os.getenv("ZABBIX_SERVER", "127.0.0.1")
 zabbix_port = int(os.getenv("ZABBIX_PORT", 10051))
 zabbix_metric_hostname = os.getenv("ZABBIX_METRIC_HOST","Zabbix Server")
-zabbix_metric_key = os.getenv("ZABBIX_KETRIC_KEY", "speedtest[fastdotcom]")
+zabbix_metric_key = os.getenv("ZABBIX_METRIC_KEY", "speedtest[fastdotcom]")
 
 
 if __name__ == '__main__':
@@ -29,7 +30,10 @@ if __name__ == '__main__':
             speed = fastdotcom.fast_com(maxtime=test_duration)
             date = time.strftime(date_format)
             packet = [ ZabbixMetric(zabbix_metric_hostname, zabbix_metric_key, speed)]
-            result = ZabbixSender(zabbix_server, zabbix_port).send(packet)
+            try:
+                result = ZabbixSender(zabbix_server, zabbix_port).send(packet)
+            except socket_error as serr:
+                result = serr
             output = log_format.format(date, speed, result)
 
             with open(output_file, "a+") as f:
